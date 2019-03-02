3e9bc7299806d91e2bde5a65f2be333a8ab71c53
Jaime Rivera <riverajaime437@gmail.com>
2019-03-01 22:28:59 -0800

    Add comments

--- New Game 2.py
+++ New Game 2.py
@@ -3,15 +3,14 @@ import random #to get randome numbers or locations or etc..
 import sys #import the game system
 
 
-pygame.init()
+pygame.init() # new to initalize the game
 
-(width, height) = (800, 600)
-screen = pygame.display.set_mode((width, height))
-pygame.display.flip()
+(width, height) = (800, 800) # dimensions of the screen
+screen = pygame.display.set_mode((width, height)) # fucntion to display screen
+pygame.display.flip() # call pygame to display screen
 
-while True:
-    pass
-
-
-
-game_over = False
+running = True # to open screen and to close
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
