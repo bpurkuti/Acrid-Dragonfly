530d7554b0705eded1805e1977dbf408ead87e5f
Jaime Rivera <riverajaime437@gmail.com>
2019-03-02 01:07:29 -0800

    NEW

--- Randome.py
+++ Randome.py
@@ -4,17 +4,16 @@ from pygame.locals import *
 
 pygame.init()
 
-BLACK = ( 0, 0, 0)
-WHITE = ( 255, 255, 255)
-GREEN = ( 0, 255, 0)
-RED = ( 255, 0, 0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+RED = ( 55,0,0)
 
-size = (800, 800)
+size = (800,800)
 screen = pygame.display.set_mode(size)
 screen.fill(RED)
 pygame.display.update()
 pygame.display.set_caption('The Acrid Dragon Flys')
-
 
 
 # The loop will carry on until the user exit the game (e.g. clicks the close button).
