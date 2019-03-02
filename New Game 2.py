fc5035a38d5123716e9e2ba9c09eed4064ebdb77
Jaime Rivera <riverajaime437@gmail.com>
2019-03-01 22:16:58 -0800

    Add our code file

---
+++ New Game 2.py
@@ -0,0 +1,17 @@
+import pygame #always need to create a game
+import random #to get randome numbers or locations or etc..
+import sys #import the game system
+
+
+pygame.init()
+
+(width, height) = (800, 600)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+
+while True:
+    pass
+
+
+
+game_over = False
