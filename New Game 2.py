7bbda4278a26e3be912f7ea6fc7e029d65a46eff
Jaime Rivera <riverajaime437@gmail.com>
2019-03-01 22:40:40 -0800

    New

--- New Game 2.py
+++ New Game 2.py
@@ -5,9 +5,17 @@ import sys #import the game system
 
 pygame.init() # new to initalize the game
 
+background_colour = (240,255,255) # background_colour - color of screen
 (width, height) = (800, 800) # dimensions of the screen
+
+
 screen = pygame.display.set_mode((width, height)) # fucntion to display screen
+screen.fill(background_colour) # call the color
+pygame.display.set_caption('The Acrid Dragon Flys') # name of the game
+
+
 pygame.display.flip() # call pygame to display screen
+
 
 running = True # to open screen and to close
 while running:
