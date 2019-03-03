9f540d6b49989ce196f72531999357c4aafa0e64
EP <eshant.prakash@gmail.com>
2019-03-02 19:20:37 -0800

    Auto stash before rebase of "origin/master"
    fixed keypress

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ p1= player(300,410,64,64)
 running = True
 while running:
     #FPS
-    clock.tick(50)
+    clock.tick(30)
 
     screen.fill((24,24,24))
 
@@ -44,36 +44,39 @@ while running:
     #drawing player
     p1.draw(screen)
 
+    keys= pygame.key.get_pressed()
+
+    #Escape to exit
+    if keys[pygame.K_ESCAPE]:
+        running = False
+    #PlayerMovements + Boundaries: Right, Left, Up, and Down
+    if (keys[pygame.K_d] or keys[pygame.K_RIGHT]):
+        if p1.x<=winX - p1.width - p1.vel:
+            p1.moveRight()
+            print ("R")
+
+    elif (keys[pygame.K_a] or keys[pygame.K_LEFT]):
+        if p1.x>=p1.vel:   
+            p1.moveLeft()
+            print ("L")
+
+    elif (keys[pygame.K_w] or keys[pygame.K_UP]):
+        if p1.y>=p1.vel:
+            p1.moveUp()
+            print ("U")
+
+    elif (keys[pygame.K_s] or keys[pygame.K_DOWN]):
+        if p1.y<=winY - p1.height - p1.vel:
+            p1.moveDown()
+            print ("D")
+    else:
+        print ("still")
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         
-        keys= pygame.key.get_pressed()
-        #Escape to exit
-        if keys[pygame.K_ESCAPE]:
-            running = False
-        #PlayerMovements + Boundaries: Right, Left, Up, and Down
-        if (keys[pygame.K_d] or keys[pygame.K_RIGHT]):
-            if p1.x<=winX - p1.width - p1.vel:
-                p1.moveRight()
-                print ("R")
-
-        elif (keys[pygame.K_a] or keys[pygame.K_LEFT]):
-            if p1.x>=p1.vel:   
-                p1.moveLeft()
-                print ("L")
-
-        elif (keys[pygame.K_w] or keys[pygame.K_UP]):
-            if p1.y>=p1.vel:
-                p1.moveUp()
-                print ("U")
-
-        elif (keys[pygame.K_s] or keys[pygame.K_DOWN]):
-            if p1.y<=winY - p1.height - p1.vel:
-                p1.moveDown()
-                print ("D")
-        else:
-            print ("still")
+        
 
     pygame.display.flip()
 
